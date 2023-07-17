a2d75733cce5b0fe22912c84b84b6010214dfad7
github user name <yokota.tomo222282@gmail.com>
2023-07-17 15:40:11 +0900

    ハードコーディング

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -63,39 +63,54 @@ const Profile = () => {
   };
 
   return (
-    <div className="p-6">
-      <h1 className="font-bold text-lg">ユーザー編集</h1>
-      <form className="space-y-3" onSubmit={handleSubmit(submit)}>
-        <h2>プロフィール画像</h2>
-        <ImageSelector control={control} name="avatarURL" />
-        <div>
-          <label className="block" htmlFor="companyName">
-            ユーザー名
-          </label>
-          <input
-            autoComplete="name"
-            {...register('username', {
-              required: '必須入力です',
-              maxLength: {
-                value: 55,
-                message: '55文字以内で入力してください',
-              },
-            })}
-            className=" border border-gray-300 text-gray-900 sm:text-sm rounded-lg  w-64 p-2.5"
-            type="text"
-            id="username"
-            placeholder="ユーザーネーム"
-          />
-          {errors.username && (
-            <p className="text-red-500 mt-0.5">{errors.username.message}</p>
-          )}
-        </div>
+    <>
+      <div className="p-6 mx-auto w-5/12">
+        <h1 className="font-bold text-lg">ユーザー編集</h1>
+        <form className="space-y-3" onSubmit={handleSubmit(submit)}>
+          <h2>プロフィール画像</h2>
+          <ImageSelector control={control} name="avatarURL" />
+          <div>
+            <label className="block" htmlFor="companyName">
+              ユーザー名
+            </label>
+            <input
+              autoComplete="name"
+              {...register('username', {
+                required: '必須入力です',
+                maxLength: {
+                  value: 55,
+                  message: '55文字以内で入力してください',
+                },
+              })}
+              className=" border border-gray-300 text-gray-900 sm:text-sm rounded-lg  w-64 p-2.5"
+              type="text"
+              id="username"
+              placeholder="ユーザーネーム"
+            />
+            {errors.username && (
+              <p className="text-red-500 mt-0.5">{errors.username.message}</p>
+            )}
+          </div>
 
-        <Button disabled={isSubmitting} type="submit">
-          編集
-        </Button>
-      </form>
-    </div>
+          <Button disabled={isSubmitting} type="submit">
+            編集
+          </Button>
+        </form>
+      </div>
+      <div className="p-6 mx-auto w-5/12">
+        <h2 className="font-bold text-lg">保存した記事</h2>
+        <div className="mt-4">
+          <article className="border border-lime-400 rounded-md p-3 w-3/5">
+            <h3 className="font-bold">title</h3>
+            <p className="text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+              labore velit perspiciatis deserunt architecto obcaecati, veritatis
+              totam,
+            </p>
+          </article>
+        </div>
+      </div>
+    </>
   );
 };
 
